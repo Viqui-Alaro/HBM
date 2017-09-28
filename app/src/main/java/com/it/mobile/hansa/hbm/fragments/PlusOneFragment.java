2c5583d8a24ccd5acc4eeef7e8073213400ed366
jcarlos79t <jcarlos79t@hotmail.com>
2017-09-28 10:44:35 -0400

    Nuevo cambio

--- app/src/main/java/com/it/mobile/hansa/hbm/fragments/PlusOneFragment.java
+++ app/src/main/java/com/it/mobile/hansa/hbm/fragments/PlusOneFragment.java
@@ -11,26 +11,9 @@ import android.view.ViewGroup;
 import com.it.mobile.hansa.hbm.R;
 import com.google.android.gms.plus.PlusOneButton;
 
-/**
- * A fragment with a Google +1 button.
- * Activities that contain this fragment must implement the
- * {@link PlusOneFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PlusOneFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class PlusOneFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    // The request code must be 0 or greater.
-    private static final int PLUS_ONE_REQUEST_CODE = 0;
-    // The URL to +1.  Must be a valid URL.
-    private final String PLUS_ONE_URL = "http://developer.android.com";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
     private PlusOneButton mPlusOneButton;
 
 
