98c2adcb733cff5440ff0f09805f7037fac6a97e
jcarlos79t <jcarlos79t@hotmail.com>
2017-09-28 11:42:40 -0400

    Con Dashboard

--- app/src/main/java/com/it/mobile/hansa/hbm/adapters/MainAdapter.java
+++ app/src/main/java/com/it/mobile/hansa/hbm/adapters/MainAdapter.java
@@ -4,10 +4,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import com.it.mobile.hansa.hbm.SampleFragment;
 import com.it.mobile.hansa.hbm.fragments.CalendarioMain;
+import com.it.mobile.hansa.hbm.fragments.DasboardMain;
 import com.it.mobile.hansa.hbm.fragments.MapaMain;
-import com.it.mobile.hansa.hbm.fragments.PlusOneFragment;
 
 /**
  * Created by JTrujillo on 25/09/2017.
@@ -34,7 +33,7 @@ public class MainAdapter extends FragmentPagerAdapter {
                 f = new CalendarioMain();
                 break;
             case 2:
-                f = new PlusOneFragment();
+                f = new DasboardMain();
                 break;
             default:
                 f = new MapaMain();
--- app/src/main/java/com/it/mobile/hansa/hbm/fragments/DasboardMain.java
+++ app/src/main/java/com/it/mobile/hansa/hbm/fragments/DasboardMain.java
@@ -1,7 +1,6 @@
 package com.it.mobile.hansa.hbm.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,30 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.it.mobile.hansa.hbm.R;
-import com.google.android.gms.plus.PlusOneButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DasboardMain extends Fragment {
 
 
-public class PlusOneFragment extends Fragment {
-
-    private PlusOneButton mPlusOneButton;
-
+    public DasboardMain() {
+        // Required empty public constructor
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_plus_one, container, false);
-
-        //Find the +1 button
-        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_dasboard_main, container, false);
     }
-
-
-
-
-
 
 }
